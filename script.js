58e3bec4a1823accf10aed8806fb03da11094064
Shyan_1943 <shyan_1943@hotmail.com>
2020-05-09 15:17:33 +0800

    script.js : axios get API & managed to print data in browser

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert('hello');
+const APIKEY = "5eb010234064fd3804165239";
+const APIURL = "https://sample-6ca3.restdb.io/rest/sailing-schedule-may-2020-soc";
+
+$(function(){
+    $('#search-btn').click(function(){
+        axios.get(APIURL, {
+            params: {
+                apikey:APIKEY
+            }
+
+        }).then(function(response){
+            let data = response.data;
+            for (let allData of data){
+                
+                $('#table-data').append(`
+                <tr>
+                    <td>${allData["POL"]}</td>
+                    <td>${allData["POD"]}</td>
+                    <td>${allData.VESSEL}</td>
+                </tr>
+                `);
+            }
+        })
+
+    });
+});
