cbf5fedd27bf47d3305ebebcbedc4b39c02854c3
Shyan_1943 <shyan_1943@hotmail.com>
2020-05-15 13:13:16 +0800

    use 'window.open(url, '_blank')' to open sailing.schedule.html in new tab

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ const APIURL = "https://sample-6ca3.restdb.io/rest/sailing-schedule-may-2020-soc
 
 $(function(){
     $('#search-btn').click(function(){
-        axios.get(APIURL, {
-            params: {
-                apikey:APIKEY
-            }
+        window.open('sailing_schedule.html','_blank');
+        // axios.get(APIURL, {
+        //     params: {
+        //         apikey:APIKEY
+        //     }
 
-        }).then(function(response){
-            let data = response.data;
-            for (let allData of data){
+        // }).then(function(response){
+        //     let data = response.data;
+        //     for (let allData of data){
                 
-                $('#table-data').append(`
-                <tr>
-                    <td>${allData["POL"]}</td>
-                    <td>${allData["POD"]}</td>
-                    <td>${allData.VESSEL}</td>
-                </tr>
-                `);
-            }
-        })
+        //         $('#table-data').append(`
+        //         <tr>
+        //             <td>${allData["POL"]}</td>
+        //             <td>${allData["POD"]}</td>
+        //             <td>${allData.VESSEL}</td>
+        //         </tr>
+        //         `);
+        //     }
+        // })
 
     });
 });
