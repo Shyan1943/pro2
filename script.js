2ad4024478aa7f31d4aeb2afaa42acb449c0b1b0
Shyan_1943 <shyan_1943@hotmail.com>
2020-05-16 15:36:40 +0800

    insert navbar

--- script.js
+++ script.js
@@ -3,25 +3,34 @@ const APIURL = "https://sample-6ca3.restdb.io/rest/sailing-schedule-may-2020-soc
 
 $(function(){
     $('#search-btn').click(function(){
-        window.open('sailing_schedule.html','_blank');
-        // axios.get(APIURL, {
-        //     params: {
-        //         apikey:APIKEY
-        //     }
+        // window.open('sailing_schedule.html','_blank');
 
-        // }).then(function(response){
-        //     let data = response.data;
-        //     for (let allData of data){
+        axios.get(APIURL, {
+            params: {
+                apikey:APIKEY
+            }
+
+        }).then(function(response){
+            let data = response.data;
+            for (let allData of data){
                 
-        //         $('#table-data').append(`
-        //         <tr>
-        //             <td>${allData["POL"]}</td>
-        //             <td>${allData["POD"]}</td>
-        //             <td>${allData.VESSEL}</td>
-        //         </tr>
-        //         `);
-        //     }
-        // })
+                // if ($('#pod')[0].value == "POD";
 
-    });
+                    $('#table-data').append(`
+                    <tr>
+                        <td>${allData["POL"]}</td>
+                        <td>${allData["POD"]}</td>
+                        <td>${allData.VESSEL}</td>
+                    </tr>
+                    `);
+            }
+        })
+
+    })
 });
+
+$(function(){
+    $('#search-btn').click(function passvalue(){
+        let 
+    })
+})
