e6780010cb031f803253ee1e4e4c7cbe8489094b
Shyan_1943 <shyan_1943@hotmail.com>
2020-05-17 14:40:25 +0800

    fixed apiURL link for pulling the correct data

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const APIURL = "https://sample-6ca3.restdb.io/rest/sailing-schedule-may-2020-soc
 
 $(function(){
     $('#search-btn').click(function(){
-        // window.open('sailing_schedule.html','_blank');
-
         axios.get(APIURL, {
             params: {
                 apikey:APIKEY
             }
-
         }).then(function(response){
             let data = response.data;
+            console.log(data);
             for (let allData of data){
+                console.log(allData)
+   
                 $('#table-data').append(`
                 <tr>
                     <td>${allData["POL"]}</td>
@@ -20,9 +20,8 @@ $(function(){
                     <td>${allData.VESSEL}</td>
                 </tr>
                 `);
-            })
+            }
         })
-
-    })
+    });
 });
 
